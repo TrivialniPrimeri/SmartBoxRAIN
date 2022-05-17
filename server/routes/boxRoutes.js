aa9b8504c10e4ebb532960763a39354065e455e5
Matic Šulc <matic.sulc1@student.um.si>
2022-05-18 00:44:08 +0200

    Implementing unlock API

--- server/routes/boxRoutes.js
+++ server/routes/boxRoutes.js
@@ -2,29 +2,17 @@ var express = require('express');
 var router = express.Router();
 var boxController = require('../controllers/boxController.js');
 
-/*
- * GET
- */
 router.get('/', boxController.list);
 
-/*
- * GET
- */
 router.get('/:id', boxController.show);
 
-/*
- * POST
- */
+router.get('/unlock/:id', boxController.unlock);
+
 router.post('/', boxController.create);
 
-/*
- * PUT
- */
 router.put('/:id', boxController.update);
 
-/*
- * DELETE
- */
 router.delete('/:id', boxController.remove);
+
 
 module.exports = router;
